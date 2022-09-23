fcd99691a9d879e410a11c413a1bdcc9c7ec8d7a
justin-behm2016 <111502918+justin-behm2016@users.noreply.github.com>
2022-09-23 06:03:07 -0700

    Add files via upload

---
+++ queries/Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+
+--Creating retiring employees by title tables.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)	
+	WHERE (e.birth_date BETWEEN '1952-01-01' AND  '1955-12-31')
+	ORDER BY e.emp_no;
+	
+SELECT DISTINCT ON (emp_no) emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+	WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, title DESC;
+
+SELECT COUNT (ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--Creating mentorship eligibility table.
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
